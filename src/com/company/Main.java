1744f2d6da8c99eafc34b60a8a0afa8f19ac20ad
Mjd hammal <mjd111.mh@gmail.com>
2018-04-14 11:50:07 +0200

    Added new functionality:
    *Game doesnt Require choosing X or O on Each turn. Game starts with X.
    
    *Game doesnt allow choosing a block that is alreay Taken and will loop until an empty block is chosen.
    
    *Game Gives you the option to Play again after Each Game.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,37 @@
 package com.company;
 
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> blocks = new ArrayList<>();
-        blocks.add("[   ]");
-        blocks.add("[   ]");
-        blocks.add("[   ]");
-        blocks.add("[   ]");
-        blocks.add("[   ]");
-        blocks.add("[   ]");
-        blocks.add("[   ]");
-        blocks.add("[   ]");
-        blocks.add("[   ]");
-        blocks.add("[   ]");
-        usersInput(blocks);
-        System.out.println("Game Over. Thanks for playing :)");
 
+        while (true) {
+            ArrayList<String> blocks = new ArrayList<>();
+            blocks.add("[   ]");
+            blocks.add("[   ]");
+            blocks.add("[   ]");
+            blocks.add("[   ]");
+            blocks.add("[   ]");
+            blocks.add("[   ]");
+            blocks.add("[   ]");
+            blocks.add("[   ]");
+            blocks.add("[   ]");
+            blocks.add("[   ]");
+            usersInput(blocks);
+            Scanner input = new Scanner(System.in);
+            String playAgain = "";
+            while (!playAgain.equalsIgnoreCase("yes") && !playAgain.equalsIgnoreCase("no")) {
+                System.out.println("please Enter Yes or No: ");
+                playAgain = input.nextLine();
+            }
+            if (playAgain.equalsIgnoreCase("no")) {
+                System.out.println("Game Over. Thanks for playing. :)");
+                break;
+            }
+        }
     }
 
     public static void usersInput(ArrayList<String> blocks) {
@@ -27,28 +39,44 @@ public class Main {
 
         int i = 0;
         while (i < blocks.size() - 1) {
-            System.out.print("Please choose X or O: ");
-            String charinput = "[ " + userInput.nextLine() + " ]";
-            System.out.print("Please choose block: ");
-            int intinput = userInput.nextInt();
-            userInput.nextLine();
-            blocks.set(intinput, charinput);
+            String charinput = "";
+            if (i % 2 == 0) {
+                charinput = "[ X ]";
+            } else {
+                charinput = "[ O ]";
+            }
+            while (true) {
+
+                System.out.print("Please choose block: ");
+                int intinput = userInput.nextInt();
+                userInput.nextLine();
+
+                   if (!blocks.get(intinput).equalsIgnoreCase("[   ]")) {
+                     continue;
+                     }
+
+                blocks.set(intinput, charinput);
+                break;
+
+            }
+
             System.out.println(blocks.get(7) + " " + blocks.get(8) + " " + blocks.get(9));
             System.out.println(blocks.get(4) + " " + blocks.get(5) + " " + blocks.get(6));
             System.out.println(blocks.get(1) + " " + blocks.get(2) + " " + blocks.get(3));
             i++;
-            boolean x = xChecker(blocks);
-            boolean o = oChecker(blocks);
-            if (x == true) {
+            boolean x = wChecker(blocks, "x");
+            boolean o = wChecker(blocks, "o");
+
+            if (x) {
                 System.out.println("X Wins.");
                 i = blocks.size() - 1;
 
             }
-            if (o == true) {
+            if (o) {
                 System.out.println("O Wins.");
                 i = blocks.size() - 1;
-
             }
+
             if ((i == blocks.size() - 1) && (o == false && x == false)) {
                 System.out.println("Its a Draw.");
             }
@@ -56,62 +84,31 @@ public class Main {
 
     }
 
-    public static boolean xChecker(ArrayList<String> blocks) {
-        if (blocks.get(7).equalsIgnoreCase("[ x ]") && blocks.get(8).equalsIgnoreCase("[ x ]") && blocks.get(9).equalsIgnoreCase("[ x ]")) {
-            //System.out.println("Player X Wins this game.");
+    public static boolean wChecker(ArrayList<String> blocks, String winner) {
+        String winning = "[ " + winner + " ]";
+        if (blocks.get(7).equalsIgnoreCase(winning) && blocks.get(8).equalsIgnoreCase(winning) && blocks.get(9).equalsIgnoreCase(winning)) {
             return true;
         }
 
-        if (blocks.get(4).equalsIgnoreCase("[ x ]") && blocks.get(5).equalsIgnoreCase("[ x ]") && blocks.get(6).equalsIgnoreCase("[ x ]")) {
+        if (blocks.get(4).equalsIgnoreCase(winning) && blocks.get(5).equalsIgnoreCase(winning) && blocks.get(6).equalsIgnoreCase(winning)) {
             return true;
         }
-        if (blocks.get(1).equalsIgnoreCase("[ x ]") && blocks.get(2).equalsIgnoreCase("[ x ]") && blocks.get(3).equalsIgnoreCase("[ x ]")) {
+        if (blocks.get(1).equalsIgnoreCase(winning) && blocks.get(2).equalsIgnoreCase(winning) && blocks.get(3).equalsIgnoreCase(winning)) {
             return true;
         }
-        if (blocks.get(9).equalsIgnoreCase("[ x ]") && blocks.get(5).equalsIgnoreCase("[ x ]") && blocks.get(1).equalsIgnoreCase("[ x ]")) {
+        if (blocks.get(9).equalsIgnoreCase(winning) && blocks.get(5).equalsIgnoreCase(winning) && blocks.get(1).equalsIgnoreCase(winning)) {
             return true;
         }
-        if (blocks.get(3).equalsIgnoreCase("[ x ]") && blocks.get(5).equalsIgnoreCase("[ x ]") && blocks.get(7).equalsIgnoreCase("[ x ]")) {
+        if (blocks.get(3).equalsIgnoreCase(winning) && blocks.get(5).equalsIgnoreCase(winning) && blocks.get(7).equalsIgnoreCase(winning)) {
             return true;
         }
-        if (blocks.get(7).equalsIgnoreCase("[ x ]") && blocks.get(4).equalsIgnoreCase("[ x ]") && blocks.get(1).equalsIgnoreCase("[ x ]")) {
+        if (blocks.get(7).equalsIgnoreCase(winning) && blocks.get(4).equalsIgnoreCase(winning) && blocks.get(1).equalsIgnoreCase(winning)) {
             return true;
         }
-        if (blocks.get(8).equalsIgnoreCase("[ x ]") && blocks.get(5).equalsIgnoreCase("[ x ]") && blocks.get(2).equalsIgnoreCase("[ x ]")) {
+        if (blocks.get(8).equalsIgnoreCase(winning) && blocks.get(5).equalsIgnoreCase(winning) && blocks.get(2).equalsIgnoreCase(winning)) {
             return true;
         }
-        if (blocks.get(9).equalsIgnoreCase("[ x ]") && blocks.get(6).equalsIgnoreCase("[ x ]") && blocks.get(3).equalsIgnoreCase("[ x ]")) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static boolean oChecker(ArrayList<String> blocks) {
-        if (blocks.get(7).equalsIgnoreCase("[ o ]") && blocks.get(8).equalsIgnoreCase("[ o ]") && blocks.get(9).equalsIgnoreCase("[ o ]")) {
-            //System.out.println("Player X Wins this game.");
-            return true;
-        }
-
-        if (blocks.get(4).equalsIgnoreCase("[ o ]") && blocks.get(5).equalsIgnoreCase("[ o ]") && blocks.get(6).equalsIgnoreCase("[ o ]")) {
-            return true;
-        }
-        if (blocks.get(1).equalsIgnoreCase("[ o ]") && blocks.get(2).equalsIgnoreCase("[ o ]") && blocks.get(3).equalsIgnoreCase("[ o ]")) {
-            return true;
-        }
-        if (blocks.get(9).equalsIgnoreCase("[ o ]") && blocks.get(5).equalsIgnoreCase("[ o ]") && blocks.get(1).equalsIgnoreCase("[ o ]")) {
-            return true;
-        }
-        if (blocks.get(3).equalsIgnoreCase("[ o ]") && blocks.get(5).equalsIgnoreCase("[ o ]") && blocks.get(7).equalsIgnoreCase("[ o ]")) {
-            return true;
-        }
-        if (blocks.get(7).equalsIgnoreCase("[ o ]") && blocks.get(4).equalsIgnoreCase("[ o ]") && blocks.get(1).equalsIgnoreCase("[ o ]")) {
-            return true;
-        }
-        if (blocks.get(8).equalsIgnoreCase("[ o ]") && blocks.get(5).equalsIgnoreCase("[ o ]") && blocks.get(2).equalsIgnoreCase("[ o ]")) {
-            return true;
-        }
-        if (blocks.get(9).equalsIgnoreCase("[ o ]") && blocks.get(6).equalsIgnoreCase("[ o ]") && blocks.get(3).equalsIgnoreCase("[ o ]")) {
+        if (blocks.get(9).equalsIgnoreCase(winning) && blocks.get(6).equalsIgnoreCase(winning) && blocks.get(3).equalsIgnoreCase(winning)) {
             return true;
         } else {
             return false;
@@ -120,6 +117,8 @@ public class Main {
 
 
 }
+
+
 
 
 
